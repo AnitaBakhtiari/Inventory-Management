b8ab14042d5eab0924b1ab5f68fbfd3c4c5cb53c
Anita Bakhtiari <60385546+AnitaBakhtiari@users.noreply.github.com>
2024-11-06 11:18:21 +0330

    Add EntityType Configuration For IssuanceDocument (#25)
    * Reset last migration for cleaner schema structure
    
    * Move UnitOfWork to a separate file for better organization
    
    * Configure Guid primary key to use NEWSEQUENTIALID() in SQL Server
    
    * Update migration

---
+++ src/Infrastructure/Persistence/Domain/IssuanceDocumentConfiguration/IssuanceDocumentTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManagement.Domain.IssuanceDocuments;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryManagement.Infrastructure.Persistence.Domain.IssuanceDocumentConfiguration
+{
+    public class IssuanceDocumentTypeConfiguration : IEntityTypeConfiguration<IssuanceDocument>
+    {
+        public void Configure(EntityTypeBuilder<IssuanceDocument> builder)
+        {
+            builder
+           .Property(e => e.Id)
+           .HasDefaultValueSql("NEWSEQUENTIALID()");
+        }
+    }
+}
